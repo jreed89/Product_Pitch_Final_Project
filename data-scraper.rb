4fa9b7c7a6a2f2341193787e101e5c4c78bdf918
Joshua Daniel <jrdaniel@ncsu.edu>
2016-08-12 12:16:28 -0400

    first gh-page commit

---
+++ data-scraper.rb
@@ -0,0 +1,21 @@
+require "open-uri"
+require "Nokogiri"
+
+
+
+doc = Nokogiri::HTML(File.open("b_chain.html")) do |config|
+config.options = Nokogiri::XML::ParseOptions::NOBLANKS
+end
+
+# puts doc
+
+puts tableData = doc.css("table")
+
+# puts allTitles = doc.css('td li i').text
+
+
+
+# allTitles = doc.css('td i').each do |item|
+#   title = item.at_css('a').text
+#   puts "Titles is: #{title}"
+# end
