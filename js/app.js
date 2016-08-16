94a794d318415f69b27aa094df0805e2cf270bde
Joshua Daniel <jrdaniel@ncsu.edu>
2016-08-15 22:31:43 -0400

    finished pitch

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
-// Code addapted from Marius Craciunoiu
-// https://medium.com/@mariusc23
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
-// on scroll, let the interval function know the user has scrolled
-$(window).scroll(function(event){
-  didScroll = true;
-  console.log(didScroll)
-});
-// run hasScrolled() and reset didScroll status
-setInterval(function() {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-  }
-}, 250);
-function hasScrolled() {
-  // do stuff here...
-  var scroll_position = $(this).scrollTop();
-  if (Math.abs(lastScrollTop - scroll_position) <= delta)
-  return;
-  // If current position > last position AND scrolled past navbar...
-  if (scroll_position > lastScrollTop && scroll_position > navbarHeight){
-    // Scroll Down
-    $('nav').removeClass('nav-down').addClass('nav-up');
-  } else {
-    // Scroll Up
-    // If did not scroll past the document (possible on mac)...
-    if(scroll_position + $(window).height() < $(document).height()) {
-      $('nav').removeClass('nav-up').addClass('nav-down');
-    }
-  }
-  lastScrollTop = scroll_position;
-}
+(function($){
+    $(function(){
+
+      $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+          }
+        );
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+  var mobile = document.createElement('div');
+  mobile.className = 'nav-mobile';
+  document.querySelector('.nav').appendChild(mobile);
